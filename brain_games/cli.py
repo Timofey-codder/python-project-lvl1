2bdbfbfc43f0f67bedb1933155ec6ca47c676be2
Timofey-codder <timsvel@gmail.com>
2021-04-18 01:27:07 +0300

    biglinterfix!

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -25,10 +25,14 @@ def game_numbers(name):
         print("Question: ", question)
         answer = prompt.string('Your answer: ')
 
-        if (question % 2 == 0 and answer == 'yes') or (question % 2 == 1 and answer == 'no'):
+        if (
+            question % 2 == 0 and answer == 'yes') or (
+                question % 2 == 1 and answer == 'no'):
             print("Correct!")
         else:
-            print("'" + answer + "' is wrong answer ;(. Correct answer was '" + true_answer + "'.")
+            print(
+                "'" + answer + "' is wrong answer "
+                ";(. Correct answer was '" + true_answer + "'.")
             print("Let's try again, " + name + "!")
             return None
 
@@ -37,30 +41,30 @@ def game_numbers(name):
 
 def calc(name):
     print("What is the result of the expression?")
-    symbol = random.randint(1,3)
+    symbol = random.randint(1, 3)
 
     for number in range(3):
         print("Question:", end=" ")
         if symbol == 1:
-            first = random.randint(1,99)
-            second = random.randint(1,99)
+            first = random.randint(1, 99)
+            second = random.randint(1, 99)
             print(str(first) + " + " + str(second))
             result = first + second
 
         elif symbol == 2:
-            first = random.randint(1,99)
-            second = random.randint(1,99)
+            first = random.randint(1, 99)
+            second = random.randint(1, 99)
 
             while first < second:
-                first = random.randint(1,99)
-                second = random.randint(1,99)
+                first = random.randint(1, 99)
+                second = random.randint(1, 99)
 
             print(str(first) + " - " + str(second))
             result = first - second
 
         else:
-            first = random.randint(1,99)
-            second = random.randint(1,99)
+            first = random.randint(1, 99)
+            second = random.randint(1, 99)
             print(str(first) + " * " + str(second))
             result = first * second
 
@@ -68,7 +72,9 @@ def calc(name):
         if str(result) == answer:
             print("Correct!")
         else:
-            print("'" + answer + "' is wrong answer ;(. Correct answer was '" + str(result) + "'.")
+            print(
+                "'" + answer + "' is wrong answer ;(. "
+                "Correct answer was '" + str(result) + "'.")
             print("Let's try again, " + name + "!")
             return None
 
@@ -79,18 +85,20 @@ def gcd_calc(name):
     print("Find the greatest common divisor of given numbers.")
 
     for number in range(3):
-        first = random.randint(1,99)
-        second = random.randint(1,99)
+        first = random.randint(1, 99)
+        second = random.randint(1, 99)
         result = gcd(first, second)
-        print ("Question:", first, second)
+        print("Question:", first, second)
         answer = prompt.string('Your answer: ')
         if str(result) == answer:
             print("Correct!")
         else:
-            print("'" + answer +"' is wrong answer ;(. Correct answer was '" + str(result) + "'.")
+            print(
+                "'" + answer + "' is wrong answer ;(. "
+                "Correct answer was '" + str(result) + "'.")
             print("Let's try again, " + name + "!")
             return None
-        
+
     print("Congratulations, " + name + "!")
 
 
@@ -99,11 +107,11 @@ def progression(name):
 
     ind = 0
     while ind < 3:
-        count = random.randint(5,10)
-        differ = random.randint(1,25)
-        pos = random.randint(1,count) - 1
-        first = random.randint(1,99)
-        print ("Question: ", end='')
+        count = random.randint(5, 10)
+        differ = random.randint(1, 25)
+        pos = random.randint(1, count) - 1
+        first = random.randint(1, 99)
+        print("Question: ", end='')
 
         for number in range(count):
             if number == pos:
@@ -111,7 +119,7 @@ def progression(name):
                 result = first
             else:
                 print(str(first), end=' ')
-            
+
             first = first + differ
 
         print('')
@@ -120,7 +128,9 @@ def progression(name):
         if str(result) == answer:
             print("Correct!")
         else:
-            print("'" + answer +"' is wrong answer ;(. Correct answer was '" + str(result) + "'.")
+            print(
+                "'" + answer + "' is wrong answer ;(. "
+                "Correct answer was '" + str(result) + "'.")
             print("Let's try again, " + name + "!")
             return None
 
@@ -133,11 +143,11 @@ def primer(name):
     print("Answer \"yes\" if given number is prime. Otherwise answer \"no\".")
 
     for number in range(3):
-        count = random.randint(1,99)
+        count = random.randint(1, 99)
         print("Question:", count)
         answer = prompt.string('Your answer: ')
 
-        if isPrime(count) == True:
+        if isPrime(count) is True:
             result = 'yes'
         else:
             result = 'no'
@@ -145,16 +155,18 @@ def primer(name):
         if result == answer:
             print("Correct!")
         else:
-            print("'" + answer +"' is wrong answer ;(. Correct answer was '" + result + "'.")
+            print(
+                "'" + answer + "' is wrong answer ;(. "
+                "Correct answer was '" + result + "'.")
             print("Let's try again, " + name + "!")
             return None
-        
+
     print("Congratulations, " + name + "!")
 
 
 def isPrime(n):
-    for i in range(2,int(n**0.5)+1):
-        if n%i==0:
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
             return False
-        
+
     return True
