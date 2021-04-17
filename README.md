46c314dbecff5862f109e1e9b8b37af7497e1f0e
Timofey-codder <timsvel@gmail.com>
2021-04-17 22:53:50 +0300

    calc

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ https://asciinema.org/a/pJ1bJDzgpC3gE3cNA0uA5gUBE
 
 <a href="https://codeclimate.com/github/codeclimate/codeclimate/maintainability"><img src="https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability" /></a>
 >>>>>>> 7c85a24... brain-even
+https://asciinema.org/a/8gl8j4V7Un4StpgzlddPepefa
--- brain_games/cli.py
+++ brain_games/cli.py
@@ -30,4 +30,46 @@ def game_numbers(name):
             print("'" + answer + "' is wrong answer ;(. Correct answer was '" + true_answer + "'.")
             print("Let's try again, " + name + "!")
             return None
+
     print("Congratulations, " + name + "!")
+
+
+def calc(name):
+    print("What is the result of the expression?")
+    symbol = random.randint(1,3)
+
+    for number in range(3):
+        print("Question:", end=" ")
+        if symbol == 1:
+            first = random.randint(1,99)
+            second = random.randint(1,99)
+            print(str(first) + " + " + str(second))
+            result = first + second
+
+        elif symbol == 2:
+            first = random.randint(1,99)
+            second = random.randint(1,99)
+
+            while first < second:
+                first = random.randint(1,99)
+                second = random.randint(1,99)
+
+            print(str(first) + " - " + str(second))
+            result = first - second
+
+        else:
+            first = random.randint(1,99)
+            second = random.randint(1,99)
+            print(str(first) + " * " + str(second))
+            result = first * second
+
+        answer = prompt.string('Your answer: ')
+        if str(result) == answer:
+            print("Correct!")
+        else:
+            print("'" + answer + "' is wrong answer ;(. Correct answer was '" + str(result) + "'.")
+            print("Let's try again, " + name + "!")
+            return None
+
+    print("Congratulations, " + name + "!")
+
